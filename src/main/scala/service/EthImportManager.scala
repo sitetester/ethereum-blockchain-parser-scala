a695ddbb85af021f29e865d7b94de04d713e84fe
sitetester <sitetester2010@gmail.com>
2020-01-10 20:37:39 +0200

    timing added to debug purpose

--- src/main/scala/service/EthImportManager.scala
+++ src/main/scala/service/EthImportManager.scala
@@ -1,6 +1,10 @@
 package service
 
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 import service.parser.EthBlocksParser
+
 object EthImportManager {
 
   // It will save parsed data into DB
@@ -10,11 +14,16 @@ object EthImportManager {
       val lastScannedBlockNumber = db.DbHelper.getLastBlockNumber
 
       val fromBlockNumber = lastScannedBlockNumber.getOrElse(0) + 1
-      val tillBlockNumber = if (fromBlockNumber == 0) 50 else 49
+      val tillBlockNumber = if (fromBlockNumber == 0) 10 else 9
       val blockNumbers = (fromBlockNumber to (fromBlockNumber + tillBlockNumber)).toList
 
       new EthBlocksParser().parseBlocks(blockNumbers)
-      println("done with block#: " + blockNumbers.last)
+
+      val currentTime = DateTimeFormatter
+        .ofPattern("HH:mm:ss")
+        .format(LocalDateTime.now)
+
+      println(currentTime + " - done with block#: " + blockNumbers.last)
     }
   }
 }
