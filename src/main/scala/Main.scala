f1b525b5846aa4a743db79b094a6b1cf11258334
sitetester <sitetester2010@gmail.com>
2020-01-10 18:37:59 +0200

    useless stuff removed

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,7 @@
 import service.EthImportManager
 
-import scala.collection.mutable
-
 object Main extends App {
 
   EthImportManager.manageImport()
-
-  var blocksSequence = mutable.Seq[Int]()
-  blocksSequence = blocksSequence :+ 1
-  println(blocksSequence.toList)
 
 }
